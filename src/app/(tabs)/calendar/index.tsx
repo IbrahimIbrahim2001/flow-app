cae00fe3ebebfc78561ecfde88499d0fa492fe84
IbrahimIbrahim2001 <ii8538098@gmail.com>
2026-08-06 11:11:54 +0300

    feat(calendar): create calendar layout and screen components

---
+++ src/app/(tabs)/calendar/index.tsx
@@ -0,0 +1,8 @@
+import { Text, View } from 'react-native';
+export default function CalendarScreen() {
+  return (
+    <View className="flex-1 bg-background">
+      <Text>CalendarScreen</Text>
+    </View>
+  );
+}
